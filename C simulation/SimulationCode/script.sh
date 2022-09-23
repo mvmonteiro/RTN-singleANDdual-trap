68282972684701dad1a02d0841852be28645c05d
mvmonteiro <marcus.vinim@icloud.com>
2022-09-23 14:16:09 -0300

    feat: code testing and working.

--- C simulation/SimulationCode/script.sh
+++ C simulation/SimulationCode/script.sh
@@ -5,5 +5,5 @@ range=300
 for i in `seq 0 $range`;
 do
     echo $i     # iteration print
-    gcc rtn_simple.c -o rtn -lm -std=c99 -lfftw3l && ./rtn -t $(cat t0_out.txt)     # compilation and execution of the rtn code with the new tc[0] parameter extracted from a txt file
+    gcc simu.c -o rtn -lm -std=c99 -lfftw3l && ./rtn -t $(cat t0_out.txt)     # compilation and execution of the rtn code with the new tc[0] parameter extracted from a txt file
 done
